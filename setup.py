d67a911fa07e2f1455cad9c51e4122178622c14a
Siegfried Gündert <guendert@itap.de>
2014-11-21 13:08:36 +0100

    Changed setup.py using setuptools instead distutils.

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 # -- encoding: utf-8 --
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='SciSerialize',
       version='0.0dev',
-      description='Serialize scientific data to JSON or MEssagePack',
+      description='Serialize scientific data to JSON or MessagePack.',
       author='Siegfried Guendert',
       author_email='siegfried.guendert@googlemail.com',
       url='https://github.com/SiggiGue/SciSerialize',
-      packages=['sciserialize'],
-      py_modules=['coders', 'serializers'],
+      license='MIT',
+      keywords='scientific serialize json msgpack',
+      packages=find_packages(exclude=('docs',)),
+      install_requires=['msgpack-python'],
 )
