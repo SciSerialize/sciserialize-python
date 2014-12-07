50ec4836b1e476c0055d774df47bd67a9db08a47
Siegfried Guendert <siegfried.guendert@googlemail.com>
2014-12-07 22:43:43 +0100

    Some typo and extended tests.

--- tests/README.md
+++ tests/README.md
@@ -1,6 +1,6 @@
 # Tests for SciSerialize
 ## What do we have to test?
-* All TypeCoders using?
+* All TypeCoders
 * `encode_types()` and `decode_types()`
 * `dumps()` and `loads()
 * `packb()`and `unpackb()`
--- tests/test_coders.py
+++ tests/test_coders.py
@@ -9,7 +9,18 @@ class TestTypeCoder:
         for attr in ['encode', 'decode', 'type_', 'typestr', 'verify_type']:
             assert(hasattr(self.type_coder, attr))
         assert(hasattr(self.type_coder.verify_type, '__call__'))
-
     def test_verify_type_method(self):
-        assert(self.type_coder.verify_type('test'))
+        assert(self.type_coder.verify_type('test') == True)
         assert(self.type_coder.verify_type(1) == False)
+
+class TestSetCoder:
+    set_coder = coders.SetCoder()
+    test_data = {'hello', 1}
+    def test_verifyer(self):
+        assert(self.set_coder.verify_type(self.test_data) == True)
+        assert(self.set_coder.verify_type([self.test_data]) == False)
+    def test_coder(self):
+        assert(
+            self.set_coder.decode(
+            self.set_coder.encode(
+                self.test_data)) == self.test_data)
