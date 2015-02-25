7e8e8163a5fcd4b659d90e04305933d28706b340
Siegfried Guendert <siegfried.guendert@googlemail.com>
2015-02-25 21:04:29 +0100

    Updatet setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,16 @@
 from setuptools import setup, find_packages
 
 setup(name='SciSerialize',
-      version='0.0dev',
+      version='0.1alpha',
       description='Serialize scientific data to JSON or MessagePack.',
       author='Siegfried Guendert',
       author_email='siegfried.guendert@googlemail.com',
-      url='https://github.com/SiggiGue/SciSerialize',
+      url='https://github.com/SciSerialize/sciserialize-python',
       license='MIT',
       keywords='scientific serialize data exchange json msgpack',
       packages=find_packages(exclude=('docs',)),
-      install_requires=['msgpack-python'],
+      install_requires=['numpy', 'msgpack-python',
+                        'python-dateutil', 'pandas'],
       classifiers=[
           'Development Status :: 3 - Alpha',
           'Intended Audience :: Science/Research',
